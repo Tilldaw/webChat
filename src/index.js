45f9a887a717d20132cfd3233f36a8e70703f187
13226877 <13226877@qq.com>
2022-11-02 23:58:58 +0800

    在线状态和未读提醒

--- src/index.js
+++ src/index.js
@@ -4,18 +4,27 @@ const { Server } = require('socket.io')
 const http = require('http')
 const server = http.createServer(app)
 const io = new Server(server)
+const moment = require('moment')
+let person = 0
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html')
 })
+
 io.on('connection', socket => {
-  console.log('连接了', new Date())
+  console.log('上号了', moment().format('HH:mm:s'))
+  person ++
+  io.emit('onLine', person)
   socket.on('disconnect', () => {
-    console.log('离开了', new Date())
+    console.log('下线了', moment().format('HH:mm:s'))
+    person --
+    io.emit('onLine', person)
   })
   socket.on('send', (msg) => {
     io.emit('send', msg)
   })
 })
+
 server.listen(80, () => {
-  console.log('start')
+  // console.log('start')
 })
